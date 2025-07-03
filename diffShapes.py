07dd83a7520e18b822d73ade1d10e717b7ec8e2d
Devika Rajeev <145443027+devikapothiyil@users.noreply.github.com>
2025-07-03 10:15:29 +0530

    Create diffShapes.py

---
+++ diffShapes.py
@@ -0,0 +1,59 @@
+#Remove the commenting before use
+
+import turtle
+t=turtle.Turtle()
+t.shape("turtle")
+#Square
+# for _ in range(4):
+#     t.forward(100)
+#     t.right(90)
+
+#Dashed Line
+# for i in range(50):
+#     t.forward(10)
+#     t.penup()
+#     t.forward(10)
+#     t.pendown()
+
+#Triangle
+# for i in range(3):
+#     t.forward(100)
+#     t.left(120)
+
+#Pentagon
+# for i in range(5):
+#     t.forward(100)
+#     t.left(72)
+
+#Hexagon
+# for i in range(6):
+#     t.forward(100)
+#     t.left(60)
+
+#Star
+# for i in range(5):
+#     t.forward(200)
+#     t.left(144)
+
+#Heptagon
+# for i in range(7):
+#     t.forward(100)
+#     t.left(360/7)
+
+#Ocatgon
+# for i in range(8):
+#     t.forward(100)
+#     t.left(360/8)
+
+#Nonagon
+# for i in range(9):
+#     t.forward(100)
+#     t.left(360/9)
+
+#Decagon
+# for i in range(10):
+#     t.forward(100)
+#     t.left(360/10)
+
+screen=turtle.Screen()
+screen.exitonclick()
